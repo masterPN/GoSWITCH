37c98bc4f8cf2015cf9787469c76fd9474c449f0
masterPN <69779985+masterPN@users.noreply.github.com>
2024-08-04 21:41:47 +0700

    impl post radiusOnestageValidate endpoint

--- mssql-service/internal/server/routes.go
+++ mssql-service/internal/server/routes.go
@@ -10,6 +10,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
 
 	r.GET("/", s.HelloWorldHandler)
+	r.POST("/radiusOnestageValidate", s.ExecuteRadiusOnestageValidateHandler)
 
 	return r
 }
@@ -19,4 +20,23 @@ func (s *Server) HelloWorldHandler(c *gin.Context) {
 	resp["message"] = "Hello World"
 
 	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) ExecuteRadiusOnestageValidateHandler(c *gin.Context) {
+	var input struct {
+		Prefix            int `json:"prefix"`
+		CallingNumber     int `json:"callingNumber"`
+		DestinationNumber int `json:"destinationNumber"`
+	}
+	c.BindJSON(&input)
+
+	result, err := s.models.RadiusData.ExecuteRadiusOnestageValidate(input.Prefix, input.CallingNumber, input.DestinationNumber)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
 }
