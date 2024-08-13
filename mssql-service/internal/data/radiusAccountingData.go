5468c8d5a75a0a141274c0b8d161d09e1c43a2f2
masterPN <69779985+masterPN@users.noreply.github.com>
2024-08-14 01:04:52 +0700

    change time json to string

--- mssql-service/internal/data/radiusAccountingData.go
+++ mssql-service/internal/data/radiusAccountingData.go
@@ -13,23 +13,23 @@ type RadiusAccountingData struct {
 }
 
 type RadiusAccountingInput struct {
-	ConfID       int       `json:"confID"`
-	AccessNo     string    `json:"accessNo"`
-	Anino        string    `json:"anino"`
-	DestNo       string    `json:"destNo"`
-	SubscriberNo string    `json:"subscriberNo"`
-	Pwd          string    `json:"pwd"`
-	SessionID    string    `json:"sessionID"`
-	CategoryID   string    `json:"categoryID"`
-	StartTime    time.Time `json:"startTime"`
-	TalkingTime  time.Time `json:"talkingTime"`
-	CallDuration int       `json:"callDuration"`
-	ReleaseCode  string    `json:"releaseCode"`
-	InTrunkID    int       `json:"inTrunkID"`
-	OutTrunkID   int       `json:"outTrunkID"`
-	ReasonID     int       `json:"reasonID"`
-	Prefix       string    `json:"prefix"`
-	LanguageCode string    `json:"languageCode"`
+	ConfID       int    `json:"confID"`
+	AccessNo     string `json:"accessNo"`
+	Anino        string `json:"anino"`
+	DestNo       string `json:"destNo"`
+	SubscriberNo string `json:"subscriberNo"`
+	Pwd          string `json:"pwd"`
+	SessionID    string `json:"sessionID"`
+	CategoryID   string `json:"categoryID"`
+	StartTime    string `json:"startTime"`
+	TalkingTime  string `json:"talkingTime"`
+	CallDuration int    `json:"callDuration"`
+	ReleaseCode  string `json:"releaseCode"`
+	InTrunkID    int    `json:"inTrunkID"`
+	OutTrunkID   int    `json:"outTrunkID"`
+	ReasonID     int    `json:"reasonID"`
+	Prefix       string `json:"prefix"`
+	LanguageCode string `json:"languageCode"`
 }
 
 type RadiusAccountingDataModel struct {
