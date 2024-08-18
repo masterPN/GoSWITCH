225c0f3f2ff07618374c2bda426abb07978039b2
masterPN <69779985+masterPN@users.noreply.github.com>
2024-08-19 00:13:32 +0700

    add cases when call conference

--- esl-service/internal/app/extractMsg.go
+++ esl-service/internal/app/extractMsg.go
@@ -16,9 +16,25 @@ import (
 	. "github.com/0x19/goesl"
 )
 
+const (
+	eventCallingFunction = "Event-Calling-Function"
+)
+
 func Execute(client *Client, msg map[string]string) {
-	if strings.Contains(msg["variable_current_application_data"], "initConference") {
+	switch {
+	case strings.Contains(msg["variable_current_application_data"], "initConference"):
 		go initConferenceHandler(client, msg)
+	case strings.Contains(msg["Hangup-Cause"], "CALL_REJECTED") &&
+		strings.Contains(msg[eventCallingFunction], "switch_channel_perform_hangup"):
+		// todo Callee rejects the call
+	case strings.Contains(msg["Answer-State"], "answered") &&
+		strings.Contains(msg["Call-Direction"], "outbound") &&
+		strings.Contains(msg[eventCallingFunction], "switch_channel_perform_mark_answered"):
+		// todo Callee accepts the call
+	case strings.Contains(msg["Answer-State"], "hangup") &&
+		strings.Contains(msg["Call-Direction"], "inbound") &&
+		strings.Contains(msg[eventCallingFunction], "switch_core_session_perform_destroy"):
+		// todo Caller or Callee hangup
 	}
 }
 
