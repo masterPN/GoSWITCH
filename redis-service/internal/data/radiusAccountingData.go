eff95e6111660daeb8eaa4bab8cd717b8bc30ffc
masterPN <69779985+masterPN@users.noreply.github.com>
2024-08-23 01:51:22 +0700

    impl RadiusAccountingDataModel

---
+++ redis-service/internal/data/radiusAccountingData.go
@@ -0,0 +1,44 @@
+package data
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type RadiusAccountingInput struct {
+	AccessNo     string `json:"accessNo"`
+	Anino        string `json:"anino"`
+	DestNo       string `json:"destNo"`
+	SubscriberNo string `json:"subscriberNo"`
+	SessionID    string `json:"sessionID"`
+	StartTime    string `json:"startTime"`
+	TalkingTime  string `json:"talkingTime"`
+}
+
+type RadiusAccountingDataModel struct {
+	DB *redis.Client
+}
+
+func (r RadiusAccountingDataModel) set(input RadiusAccountingInput) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := r.DB.HSet(ctx, input.Anino, map[string]interface{}{
+		"accessNo":     input.AccessNo,
+		"anino":        input.Anino,
+		"destNo":       input.DestNo,
+		"subscriberNo": input.SubscriberNo,
+		"sessionID":    input.SessionID,
+		"startTime":    input.StartTime,
+		"talkingTime":  input.TalkingTime,
+	}).Err()
+	if err != nil {
+		log.Fatalf("Could not set hash: %v", err)
+		return err
+	}
+
+	return nil
+}
