6bb16ff22ec855e0cebdb9ebbbef858b4013a964
masterPN <69779985+masterPN@users.noreply.github.com>
2024-09-16 00:24:05 +0700

    add interface

--- mssql-service/internal/data/wholesaledata/models.go
+++ mssql-service/internal/data/wholesaledata/models.go
@@ -3,8 +3,13 @@ package wholesaledata
 import "database/sql"
 
 type Models struct {
+	ImgCdrOperatorRoutingData interface {
+		GetFirstImgCdrOperatorRoutingByNumber(number string) (ImgCdrOperatorRoutingData, error)
+	}
 }
 
 func NewModels(db *sql.DB) Models {
-	return Models{}
+	return Models{
+		ImgCdrOperatorRoutingData: ImgCdrOperatorRoutingDataModel{DB: db},
+	}
 }
