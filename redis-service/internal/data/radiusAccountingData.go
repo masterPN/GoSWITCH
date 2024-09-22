328a5f90c88e670f8f12c14072d6e0d0557811e2
masterPN <69779985+masterPN@users.noreply.github.com>
2024-09-23 03:00:00 +0700

    improve RadiusAccountingData function

--- redis-service/internal/data/radiusAccountingData.go
+++ redis-service/internal/data/radiusAccountingData.go
@@ -4,19 +4,30 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
 type RadiusAccountingData struct {
+	ConfID       int    `json:"confID"`
 	AccessNo     string `json:"accessNo"`
 	Anino        string `json:"anino"`
 	DestNo       string `json:"destNo"`
 	SubscriberNo string `json:"subscriberNo"`
+	Pwd          string `json:"pwd"`
 	SessionID    string `json:"sessionID"`
+	CategoryID   string `json:"categoryID"`
 	StartTime    string `json:"startTime"`
 	TalkingTime  string `json:"talkingTime"`
+	CallDuration int    `json:"callDuration"`
+	ReleaseCode  string `json:"releaseCode"`
+	InTrunkID    int    `json:"inTrunkID"`
+	OutTrunkID   int    `json:"outTrunkID"`
+	ReasonID     int    `json:"reasonID"`
+	Prefix       string `json:"prefix"`
+	LanguageCode string `json:"languageCode"`
 }
 
 type RadiusAccountingDataModel struct {
@@ -28,13 +39,23 @@ func (r RadiusAccountingDataModel) Set(input RadiusAccountingData) error {
 	defer cancel()
 
 	err := r.DB.HSet(ctx, input.Anino, map[string]interface{}{
+		"confID":       input.ConfID,
 		"accessNo":     input.AccessNo,
 		"anino":        input.Anino,
 		"destNo":       input.DestNo,
 		"subscriberNo": input.SubscriberNo,
+		"pwd":          input.Pwd,
 		"sessionID":    input.SessionID,
+		"categoryID":   input.CategoryID,
 		"startTime":    input.StartTime,
 		"talkingTime":  input.TalkingTime,
+		"callDuration": input.CallDuration,
+		"releaseCode":  input.ReleaseCode,
+		"inTrunkID":    input.InTrunkID,
+		"outTrunkID":   input.OutTrunkID,
+		"reasonID":     input.ReasonID,
+		"prefix":       input.Prefix,
+		"languageCode": input.LanguageCode,
 	}).Err()
 	if err != nil {
 		log.Fatalf("Could not set hash: %v", err)
@@ -55,14 +76,31 @@ func (r RadiusAccountingDataModel) Pop(anino string) (RadiusAccountingData, erro
 		return RadiusAccountingData{}, err
 	}
 
+	// Convert string fields to appropriate types
+	confID, _ := strconv.Atoi(radiusAccountingDataMap["confID"])
+	callDuration, _ := strconv.Atoi(radiusAccountingDataMap["callDuration"])
+	inTrunkID, _ := strconv.Atoi(radiusAccountingDataMap["inTrunkID"])
+	outTrunkID, _ := strconv.Atoi(radiusAccountingDataMap["outTrunkID"])
+	reasonID, _ := strconv.Atoi(radiusAccountingDataMap["reasonID"])
+
 	radiusAccountingData := RadiusAccountingData{
+		ConfID:       confID,
 		AccessNo:     radiusAccountingDataMap["accessNo"],
 		Anino:        radiusAccountingDataMap["anino"],
 		DestNo:       radiusAccountingDataMap["destNo"],
 		SubscriberNo: radiusAccountingDataMap["subscriberNo"],
+		Pwd:          radiusAccountingDataMap["pwd"],
 		SessionID:    radiusAccountingDataMap["sessionID"],
+		CategoryID:   radiusAccountingDataMap["categoryID"],
 		StartTime:    radiusAccountingDataMap["startTime"],
 		TalkingTime:  radiusAccountingDataMap["talkingTime"],
+		CallDuration: callDuration,
+		ReleaseCode:  radiusAccountingDataMap["releaseCode"],
+		InTrunkID:    inTrunkID,
+		OutTrunkID:   outTrunkID,
+		ReasonID:     reasonID,
+		Prefix:       radiusAccountingDataMap["prefix"],
+		LanguageCode: radiusAccountingDataMap["languageCode"],
 	}
 
 	// Delete the key
@@ -78,7 +116,7 @@ func (r RadiusAccountingDataModel) Pop(anino string) (RadiusAccountingData, erro
 		fmt.Printf("Key %s was deleted successfully.\n", anino)
 	} else {
 		fmt.Printf("Key %s does not exist.\n", anino)
-		return radiusAccountingData, fmt.Errorf("Key %s does not exist.\n", anino)
+		return radiusAccountingData, fmt.Errorf("key %s does not exist", anino)
 	}
 
 	return radiusAccountingData, nil
