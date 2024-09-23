6336454ba7585046115c00bee941341e50c70f86
masterPN <69779985+masterPN@users.noreply.github.com>
2024-09-23 21:43:11 +0700

    change radius accounting key

--- redis-service/internal/data/radiusAccountingData.go
+++ redis-service/internal/data/radiusAccountingData.go
@@ -38,7 +38,7 @@ func (r RadiusAccountingDataModel) Set(input RadiusAccountingData) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err := r.DB.HSet(ctx, input.Anino, map[string]interface{}{
+	err := r.DB.HSet(ctx, input.SessionID, map[string]interface{}{
 		"confID":       input.ConfID,
 		"accessNo":     input.AccessNo,
 		"anino":        input.Anino,
