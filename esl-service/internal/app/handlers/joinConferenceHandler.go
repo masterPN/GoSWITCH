7ba1b92cbb3efb062b1eadf1f454743279aaec1c
masterPN <69779985+masterPN@users.noreply.github.com>
2024-09-26 01:35:36 +0700

    make value dynamic

--- esl-service/internal/app/handlers/joinConferenceHandler.go
+++ esl-service/internal/app/handlers/joinConferenceHandler.go
@@ -14,24 +14,15 @@ const (
 )
 
 func JoinConferenceHandler(msg map[string]string) {
-	// callerNumber := msg["Caller-Caller-ID-Number"]
-	// calleeNumber := msg["Caller-Callee-ID-Number"]
-
 	startTimeUnix, _ := strconv.Atoi(msg["Caller-Channel-Created-Time"])
 	TalkingTimeUnix, _ := strconv.Atoi(msg["Caller-Channel-Answered-Time"])
 	startTime := time.UnixMicro(int64(startTimeUnix))
 	talkingTime := time.UnixMicro(int64(TalkingTimeUnix))
 
-	// todo
-	// make this param as dynamic, not dummy.
 	postBody, _ := json.Marshal(map[string]string{
-		"accessNo":     "8899",
-		"anino":        "612701681",
-		"destNo":       "66812424273",
-		"subscriberNo": "P100000000000505",
-		"sessionID":    "1723281626000100180",
-		"startTime":    startTime.Format(timeFormat),
-		"talkingTime":  talkingTime.Format(timeFormat),
+		"sessionID":   msg["variable_conference_name"],
+		"startTime":   startTime.Format(timeFormat),
+		"talkingTime": talkingTime.Format(timeFormat),
 	})
 
 	_, err := http.Post("http://redis-service:8080/saveRadiusAccountingData", "application/json", bytes.NewBuffer(postBody))
