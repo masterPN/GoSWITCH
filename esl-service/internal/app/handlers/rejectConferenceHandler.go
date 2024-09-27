f77647d8e1a0bc939bdeb39912a8d26f25bedc12
masterPN <69779985+masterPN@users.noreply.github.com>
2024-09-27 15:50:20 +0700

    using conference name

--- esl-service/internal/app/handlers/rejectConferenceHandler.go
+++ esl-service/internal/app/handlers/rejectConferenceHandler.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/0x19/goesl"
 )
 
 func RejectConferenceHandler(client *goesl.Client, msg map[string]string) {
-	client.BgApi(fmt.Sprintf("conference %v kick all", msg["Caller-Caller-ID-Number"]))
+	client.BgApi(fmt.Sprintf("conference %v kick all", msg["variable_conference_name"]))
+	http.Get(fmt.Sprintf("http://redis-service:8080/popRadiusAccountingData/%s", msg["variable_conference_name"]))
 }
