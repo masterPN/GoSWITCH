0f25d7f1fa94909e4845693522c82b056a30d54e
masterPN <69779985+masterPN@users.noreply.github.com>
2024-09-27 16:52:41 +0700

    remove unused case

--- esl-service/internal/app/extractMsg.go
+++ esl-service/internal/app/extractMsg.go
@@ -10,7 +10,6 @@ import (
 const (
 	VariableCurrentApplicationData = "variable_current_application_data"
 	EventName                      = "Event-Name"
-	HangupCause                    = "Hangup-Cause"
 	AnswerState                    = "Answer-State"
 	CallDirection                  = "Call-Direction"
 	EventCallingFunction           = "Event-Calling-Function"
@@ -20,7 +19,6 @@ func Execute(client *goesl.Client, msg map[string]string) {
 	eventName := msg[EventName]
 	eventFunction := msg[EventCallingFunction]
 	applicationData := msg[VariableCurrentApplicationData]
-	hangupCause := msg[HangupCause]
 	answerState := msg[AnswerState]
 	callDirection := msg[CallDirection]
 
@@ -28,10 +26,6 @@ func Execute(client *goesl.Client, msg map[string]string) {
 	case strings.Contains(applicationData, "initConference") && eventName == "CHANNEL_EXECUTE_COMPLETE":
 		goesl.Debug("initConference\n %q", applicationData)
 		go handlers.InitConferenceHandler(client, msg)
-	case strings.Contains(hangupCause, "CALL_REJECTED") && strings.Contains(eventFunction, "switch_channel_perform_hangup"):
-		// Callee rejects the call
-		goesl.Debug("callee rejected\n %q", msg)
-		go handlers.RejectConferenceHandler(client, msg)
 	case strings.Contains(answerState, "answered") &&
 		strings.Contains(callDirection, "outbound") &&
 		strings.Contains(eventFunction, "switch_channel_perform_mark_answered"):
