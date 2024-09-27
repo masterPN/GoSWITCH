585c6adcd938a0d2897c4f3820922138b0bb5515
masterPN <69779985+masterPN@users.noreply.github.com>
2024-09-27 15:50:29 +0700

    improve logs

--- esl-service/internal/app/extractMsg.go
+++ esl-service/internal/app/extractMsg.go
@@ -26,22 +26,22 @@ func Execute(client *goesl.Client, msg map[string]string) {
 
 	switch {
 	case strings.Contains(applicationData, "initConference") && eventName == "CHANNEL_EXECUTE_COMPLETE":
-		goesl.Debug("%q", applicationData)
+		goesl.Debug("initConference\n %q", applicationData)
 		go handlers.InitConferenceHandler(client, msg)
 	case strings.Contains(hangupCause, "CALL_REJECTED") && strings.Contains(eventFunction, "switch_channel_perform_hangup"):
 		// Callee rejects the call
-		goesl.Debug("%q", msg)
+		goesl.Debug("callee rejected\n %q", msg)
 		go handlers.RejectConferenceHandler(client, msg)
 	case strings.Contains(answerState, "answered") &&
 		strings.Contains(callDirection, "outbound") &&
 		strings.Contains(eventFunction, "switch_channel_perform_mark_answered"):
 		// Callee accepts the call
-		goesl.Debug("%q", msg)
+		goesl.Debug("callee accepted\n %q", msg)
 		go handlers.JoinConferenceHandler(msg)
 	case strings.Contains(answerState, "hangup") &&
 		strings.Contains(callDirection, "inbound") &&
 		strings.Contains(eventFunction, "switch_core_session_perform_destroy"):
-		goesl.Debug("%q", msg)
+		goesl.Debug("end conference\n %q", msg)
 		go handlers.EndConferenceHandler(client, msg)
 	}
 }
