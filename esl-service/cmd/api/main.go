6715ea73bf698075b574e6edd162261b96158bc9
masterPN <69779985+masterPN@users.noreply.github.com>
2024-09-29 23:15:27 +0700

    create eslClient as common function

--- esl-service/cmd/api/main.go
+++ esl-service/cmd/api/main.go
@@ -4,9 +4,6 @@ import (
 	"esl-service/internal/app"
 	"flag"
 	"runtime"
-	"strings"
-
-	"github.com/0x19/goesl"
 )
 
 var (
@@ -20,35 +17,19 @@ func main() {
 	// Boost it as much as it can go ...
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	client, err := goesl.NewClient(*fshost, *fsport, *password, *timeout)
-
+	client, err := app.CreateClient(*fshost, *fsport, *password, *timeout)
 	if err != nil {
-		goesl.Error("Error while creating new client: %s", err)
 		return
 	}
-
-	goesl.Debug("Yuhu! New client: %q", client)
-
-	// Apparently all is good... Let us now handle connection :)
-	// We don't want this to be inside of new connection as who knows where it my lead us.
-	// Remember that this is crutial part in handling incoming messages :)
-	go client.Handle()
-
-	client.Send("events json ALL")
+	defer client.Close()
 
 	for {
-		msg, err := client.ReadMessage()
+		msg, err := app.GetMessage(client)
 
 		if err != nil {
-
-			// If it contains EOF, we really dont care...
-			if !strings.Contains(err.Error(), "EOF") && err.Error() != "unexpected end of JSON input" {
-				goesl.Error("Error while reading Freeswitch message: %s", err)
-			}
 			break
 		}
 
-		app.Execute(client, msg.Headers)
+		app.Execute(client, msg)
 	}
-
 }
---
+++ esl-service/internal/app/eslClient.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"strings"
+
+	"github.com/0x19/goesl"
+)
+
+func CreateClient(host string, port uint, password string, timeout int) (*goesl.Client, error) {
+	client, err := goesl.NewClient(host, port, password, timeout)
+	if err != nil {
+		goesl.Error("Error while creating new client: %s", err)
+		return nil, err
+	}
+
+	goesl.Debug("Yuhu! New client: %q", client)
+
+	go client.Handle()
+	client.Send("events json ALL")
+
+	return client, nil
+}
+
+func GetMessage(client *goesl.Client) (map[string]string, error) {
+	msg, err := client.ReadMessage()
+	if err != nil {
+		if shouldLogError(err) {
+			goesl.Error("Error while reading Freeswitch message: %s", err)
+		}
+		return nil, err
+	}
+	return msg.Headers, nil
+}
+
+func shouldLogError(err error) bool {
+	errMsg := err.Error()
+	return !strings.Contains(errMsg, "EOF") && errMsg != "unexpected end of JSON input"
+}
