0f89856d13e2b1ee799d5bf70372b76fca25dc32
masterPN <69779985+masterPN@users.noreply.github.com>
2024-11-15 23:44:21 +0700

    fix cancel function

--- redis-service/internal/data/internalCodemappingData.go
+++ redis-service/internal/data/internalCodemappingData.go
@@ -52,8 +52,10 @@ func (r InternalCodemappingDataModel) populateData(data map[string]interface{}, 
 	}
 }
 
-func (r *InternalCodemappingDataModel) ClearAllRecords() error {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+func (r InternalCodemappingDataModel) ClearAllRecords() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	keys, err := r.DB.Keys(ctx, "internalCodemapping:*").Result()
 	if err != nil {
 		return err
