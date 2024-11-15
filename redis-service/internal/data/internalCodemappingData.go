37a8eacce6b9d380cef6c04c2a2aa44e5fe6a287
masterPN <69779985+masterPN@users.noreply.github.com>
2024-11-16 00:10:54 +0700

    decom delete all internalCodemapping records

--- redis-service/internal/data/internalCodemappingData.go
+++ redis-service/internal/data/internalCodemappingData.go
@@ -51,22 +51,3 @@ func (r InternalCodemappingDataModel) populateData(data map[string]interface{}, 
 		data["OperatorCode"] = strconv.Itoa(input.OperatorCode)
 	}
 }
-
-func (r InternalCodemappingDataModel) ClearAllRecords() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	keys, err := r.DB.Keys(ctx, "internalCodemapping:*").Result()
-	if err != nil {
-		return err
-	}
-
-	for _, key := range keys {
-		err = r.DB.Del(ctx, key).Err()
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
