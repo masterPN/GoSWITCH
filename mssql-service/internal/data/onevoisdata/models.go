b03a082d653859a62e58aa89487177949e97d830
masterPN <69779985+masterPN@users.noreply.github.com>
2024-11-29 01:43:19 +0700

    fix missing sharedData

--- mssql-service/internal/data/onevoisdata/models.go
+++ mssql-service/internal/data/onevoisdata/models.go
@@ -1,10 +1,14 @@
 package onevoisdata
 
-import "database/sql"
+import (
+	"database/sql"
+
+	sharedData "github.com/masterPN/GoSWITCH-shared/data"
+)
 
 type Models struct {
 	RadiusOnestageValidateData interface {
-		ExecuteRadiusOnestageValidate(prefix string, callingNumber string, destinationNumber string) (RadiusOnestageValidateData, error)
+		ExecuteRadiusOnestageValidate(prefix string, callingNumber string, destinationNumber string) (sharedData.RadiusOnestageValidateData, error)
 	}
 	RadiusAccountingData interface {
 		ExecuteRadiusAccounting(input RadiusAccounting) (RadiusAccountingData, error)
